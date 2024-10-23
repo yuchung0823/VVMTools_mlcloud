a251df150f59d1a52248969794082541f4d2530d
yuchung0823 <yuzhongzhng@gmail.com>
2024-10-23 20:01:36 +0800

    Adjust the file path in the main function and add comments to all sections of the code

--- VVManalyze.py
+++ VVManalyze.py
@@ -1,73 +1,147 @@
 from vvmtools import VVMTools
 import numpy as np
 
-import matplotlib.pyplot as plt
-from matplotlib import cm
-from matplotlib import colors
-
 class VVMTools_BL(VVMTools):
+    """
+    A subclass of VVMTools to provide additional methods specific to 
+    boundary layer calculations such as TKE, enstrophy, and boundary 
+    layer height.
+    """
     def __init__(self,case_path):
+        """
+        A subclass of VVMTools to provide additional methods specific to 
+        boundary layer calculations such as TKE, enstrophy, and boundary 
+        layer height.
+        """
         super().__init__(case_path)
     
     def calc_TKE(self, time_steps, func_config):
+        """
+        Calculate the Turbulent Kinetic Energy (TKE) using the velocity 
+        components (u, v, w).
+        
+        :param time_steps: List of time steps to compute TKE.
+        :param func_config: Configuration dictionary that includes domain range.
+        :return: Mean TKE over the domain at each time step.
+        """
+        # Get velocity components (u, v, w) for the specified time steps
         u = np.squeeze(self.get_var('u', time_steps, numpy=True, domain_range=func_config['domain_range']))
         v = np.squeeze(self.get_var('v', time_steps, numpy=True, domain_range=func_config['domain_range']))
         w = np.squeeze(self.get_var('w', time_steps, numpy=True, domain_range=func_config['domain_range'])) 
-
+        
+        # Regrid velocities to calculate TKE at cell centers
         u_regrid = (u[:, :, 1:] + u[:, :, :-1])[1:, 1:, :] / 2
         v_regrid = (v[:, 1:, :] + v[:, :-1, :])[1:, :, 1:] / 2
         w_regrid = (w[1:, :, :] + w[:-1, :, :])[:, 1:, 1:] / 2
+
+        # Calculate TKE = 0.5 * (u^2 + v^2 + w^2)
         TKE = u_regrid**2 + v_regrid**2 + w_regrid**2
 
         return np.nanmean(TKE, axis=(1,2))
     
     def calc_Enstrophy(self, time_steps, func_config):
+        """
+        Calculate enstrophy using the vorticity components (xi, eta, zeta).
+        
+        :param time_steps: List of time steps to compute enstrophy.
+        :param func_config: Configuration dictionary with domain range.
+        :return: Mean enstrophy over the domain at each time step.
+        """
+        # Get vorticity components (xi, eta, zeta)
         xi = np.squeeze(self.get_var('xi', time_steps, numpy=True, domain_range=func_config['domain_range']))
         eta = np.squeeze(self.get_var('eta', time_steps, numpy=True, domain_range=func_config['domain_range']))
+        
+        # Check if eta needs to be loaded from a different variable (eta_2)
         if xi.shape ==  eta.shape:
             pass
         else:
             eta = np.squeeze(self.get_var('eta_2', time_steps, numpy=True, domain_range=func_config['domain_range']))
         zeta = np.squeeze(self.get_var('zeta', time_steps, numpy=True, domain_range=func_config['domain_range']))
 
+        # Calculate and return mean enstrophy (xi^2 + eta^2 + zeta^2)
         return np.nanmean((xi**2+eta**2+zeta**2), axis=(1,2))
 
     def calc_w_th(self, time_steps, func_config):
+        """
+        Calculate the covariance of vertical velocity (w) and potential temperature (theta).
+        
+        :param time_steps: List of time steps to compute w'θ'.
+        :param func_config: Configuration dictionary with domain range.
+        :return: Mean w'θ' over the domain at each time step.
+        """
+        # Get vertical velocity and regrid to center points
         w = np.squeeze(self.get_var('w', time_steps, numpy=True, domain_range=func_config['domain_range']))
         w_regrid = (w[1:]+w[:-1])/2
+
+        # Calculate w' (perturbation of w)
         w_mean = np.mean(w_regrid,axis=(1,2)).reshape(w_regrid.shape[0],1,1)
         w_prime = w_regrid - w_mean
+
+        # Get potential temperature (theta) and calculate θ' (theta perturbation)
         th = np.squeeze(self.get_var('th', time_steps, numpy=True, domain_range=func_config['domain_range']))[1:]
         th_mean = np.mean(th,axis=(1,2)).reshape(th.shape[0],1,1)
         th_prime = th - th_mean
+
+        # Calculate the covariance w'θ' and return the mean over the domain
         w_th = np.mean(w_prime*th_prime,axis=(1,2))
         return w_th
     
     def h_BL_dthdz(self, time_steps, func_config):
+        """
+        Calculate the boundary layer height based on the maximum gradient of theta (dθ/dz).
+        
+        :param time_steps: List of time steps.
+        :param func_config: Configuration dictionary with domain range.
+        :return: Boundary layer height at each time step.
+        """
+        # Get the height (zc) and mean theta profile
         zc = self.get_var("zc", 0).to_numpy()/1000
         th_mean = self.get_var_parallel("th", time_steps=time_steps, domain_range=func_config['domain_range'], compute_mean=True, axis=(1, 2))
         
+        # Compute vertical gradient of theta (dθ/dz)
         dth_dz = (th_mean[:, 1:] - th_mean[:, :-1]) / (zc[1:] - zc[:-1])
-        ans = []
+        
+        # Find the height where the gradient is maximum for each time step
+        h_BL_dthdz = []
         for t in range(len(dth_dz)):
-            ans.append(zc[np.argmax(dth_dz[t])])
-        return np.array(ans)
+            h_BL_dthdz.append(zc[np.argmax(dth_dz[t])])
+        return np.array(h_BL_dthdz)
 
     def h_BL_th_plus05(self, time_steps, func_config):
+        """
+        Calculate the boundary layer height where theta exceeds the surface value by 0.5K.
+        
+        :param time_steps: List of time steps.
+        :param func_config: Configuration dictionary with domain range.
+        :return: Boundary layer height where θ exceeds surface θ by 0.5K.
+        """
+        # Get height (zc) and mean theta profile
         zc = self.get_var("zc", 0).to_numpy()/1000
+
+        # Find the height where theta exceeds surface value by 0.5K
         th_mean = self.get_var_parallel("th", time_steps=time_steps, domain_range=func_config['domain_range'], compute_mean=True, axis=(1, 2))
         th_find = th_mean - (th_mean[:, 0].reshape(th_mean.shape[0], 1) + 0.5)
 
-        ans = []
+        # Find the height where theta exceeds the surface value by 0.5K
+        h_BL_th_plus05 = []
         for t in range(len(th_find)):
-            ans.append(zc[np.where(th_find[t] < 0.01)[0][-1]])
-        return np.array(ans)
+            h_BL_th_plus05.append(zc[np.where(th_find[t] < 0.01)[0][-1]])
+        return np.array(h_BL_th_plus05)
 
     def find_BL_boundary(self, var, threshold):
+        """
+        Identify boundary layer height based on the threshold applied to the given variable.
+        
+        :param var: Variable to apply threshold on.
+        :param threshold: Threshold value for determining boundary layer height.
+        :return: Boundary layer height.
+        """
+        # Get height (zc) and apply threshold to find boundary layer
         zc = self.get_var("zc", 0).to_numpy()/1000
         positive_mask = np.swapaxes(var, 0, 1) > threshold
         index = np.argwhere(positive_mask)
         
+        # Mapping of indices to heights based on threshold crossing
         k, i = index[:, 0], index[:, 1]
         i_k_map = {}
         
@@ -78,6 +152,7 @@ class VVMTools_BL(VVMTools):
                 if k[ii] > i_k_map[i[ii]]:
                     i_k_map[i[ii]] = k[ii]
         
+        # Calculate boundary layer height for each column
         h = np.zeros(len(var))
         for key, value in i_k_map.items():
             h[key] = zc[value+1]
@@ -85,31 +160,44 @@ class VVMTools_BL(VVMTools):
         return np.array(h)
     
     def find_wth_boundary(self, var):
+        """
+        Identify three different boundary layer heights based on the vertical velocity and 
+        potential temperature covariance (w'θ').
+        
+        :param var: Covariance of w' and θ'.
+        :return: Array with lower, mid, and upper boundary heights for each time step.
+        """
         mask = np.where(var>=0,1,-1)
         zc = self.DIM['zc']/1000
         zc_wth = np.zeros((3,var.shape[0]))
         for t in range(var.shape[0]):
             temp = mask[t,1:] - mask[t,:-1]
 
+            # Default zero boundary if w'θ' is small
             if np.max(var[t])<1e-3:
                 k_lower = k_mid = k_upper = 0
 
             else:
+                # Find lower boundary
                 try:
                     k_lower = np.argwhere(temp==-2)[0][0] + 1
                 except: 
                     k_lower = 0
                 
+                # Find mid boundary
                 k_mid = np.argmin(var[t]) + 1
                 
+                # Find upper boundary
                 try:
                     k_upper = np.argwhere(temp==2)[0][0] + 1
                 except:
                     k_upper = 0
-
+            
+            # Zero upper boundary if the maximum value after the mid boundary is small
             if np.max(var[t, np.argmin(var[t]):]) < 1e-3:
                 k_upper = 0
-         
+
+            # Store the boundaries for this time step
             zc_wth[0,t], zc_wth[1,t], zc_wth[2,t] = zc[k_lower], zc[k_mid], zc[k_upper]
         
         return zc_wth
--- main.py
+++ main.py
@@ -2,61 +2,70 @@ from VVManalyze import VVMTools_BL
 from plottools import VVMPlot_BL
 import numpy as np
 
-path = lambda case:'/data/chung0823/VVM_cloud_dynamics_2024/DATA/%s/'%(case)
-case_list = ['pbl_hetero_dthdz_8','pbl_hetero_dthdz_11']
-case_names = ['S1_Coastal','S2_Coastal']
+# Define the path to the simulation data for different cases
+path = lambda case:'/home/chung0823/cloud2024/VVM_Data/%s/'%(case)
 
-path_Aaron = lambda case:'/data/mlcloud/Aaron/VVM/DATA/%s/'%(case)
-case_list_Aaron = ['OceanGrass_S1_new','OceanGrass_S1_landmid_new','OceanGrass_S2_new_new','OceanGrass_S2_landmid_newnew']
-case_names_Aaron = ['S1_Ocean','S1_Grass','S2_Ocean','S2_Grass']
+# List of simulation cases and their corresponding names
+case_list = ['pbl_hetero_dthdz_8','pbl_hetero_dthdz_11','OceanGrass_S1_new','OceanGrass_S1_landmid_new','OceanGrass_S2_new_new','OceanGrass_S2_landmid_newnew']
+case_names = ['S1_Coastal','S2_Coastal','S1_Ocean','S1_Grass','S2_Ocean','S2_Grass']
 
+# Time range for the analysis (721 time steps)
 time = np.arange(721)
 
 ############################ Hovmoller diagram ############################
+
+# Configuration for domain range, used to subset the data for analysis
 func_config={"domain_range":(0,1,None,None,None,None)}
+
+# Loop through the first 6 cases to generate Hovmöller diagrams
 for i in range(6):
+    case = case_list[i]
+    case_name = case_names[i]
 
-    if i<2:
-        case = case_list[i]
-        case_name = case_names[i]
-        myTool = VVMTools_BL(path(case))
-        Plot = VVMPlot_BL(path(case),case_name=case_name,region_name='y-axis avg')
-
-    else:
-        case = case_list_Aaron[i-2]
-        case_name = case_names_Aaron[i-2]
-        myTool = VVMTools_BL(path_Aaron(case))
-        Plot = VVMPlot_BL(path_Aaron(case),case_name=case_name,region_name='y-axis avg')
-    
-    
+    # Initialize the analysis and plotting tools
+    myTool = VVMTools_BL(path(case))
+    Plot = VVMPlot_BL(path(case),case_name=case_name,region_name='y-axis avg')
+   
+    # Retrieve NO and NO2 data and calculate their sum (NOx)
     NO = myTool.get_var_parallel('NO',time_steps=time,domain_range=func_config['domain_range'], compute_mean=True, axis=(0),cores=20)
     NO2 = myTool.get_var_parallel('NO2',time_steps=time,domain_range=func_config['domain_range'], compute_mean=True, axis=(0),cores=20)
     NOx = NO + NO2
    
+    # Plot Hovmöller diagram of NOx data
     Plot.hovmoller(data=NOx)
 
 ############################ BL height ############################
-func_configs=[{"domain_range":(None,None,None,None,None,None)}, {"domain_range":(None,None,None,None,0,64)}, {"domain_range":(None,None,None,None,64,128)}]
-region_lists=['domain','ocean','grass']
-case_name = ['S1','S2']
-for i in range(2):
+# Domain configurations for different regions (whole domain, ocean, grass)
+func_configs=[{"domain_range":(None,None,None,None,None,None)}, # Full domain
+              {"domain_range":(None,None,None,None,0,64)},      # Ocean region
+              {"domain_range":(None,None,None,None,64,128)}]    # Grass region
+region_lists=['domain','ocean','grass'] # Names of the regions for boundary layer analysis
+case_name = ['S1','S2']  # List of simulation cases corresponding names
 
+# Loop through the first two cases to analyze BL height in different regions
+for i in range(2):
     case = case_list[i]
     myTool = VVMTools_BL(path(case))
 
+    # Loop through each region (domain, ocean, grass)
     for region, func_config in zip(region_lists, func_configs):
-
+        
+        # Compute TKE (Turbulent Kinetic Energy), Enstrophy, and vertical heat flux (w'theta')
         TKE = myTool.func_time_parallel(func=myTool.calc_TKE,time_steps=time,func_config=func_config,cores=20)
         Enstrophy = myTool.func_time_parallel(func=myTool.calc_Enstrophy,time_steps=time,func_config=func_config,cores=20)
         w_th = myTool.func_time_parallel(func=myTool.calc_w_th,time_steps=time,func_config=func_config,cores=20)
 
+        # Calculate boundary layer heights based on different criteria
         h_BL_th_plus05 = myTool.h_BL_th_plus05(time_steps=time,func_config=func_config)
         h_BL_dthdz = myTool.h_BL_dthdz(time_steps=time,func_config=func_config)
         h_BL_TKE = myTool.find_BL_boundary(TKE,threshold=0.08)
         h_BL_Enstrophy = myTool.find_BL_boundary(Enstrophy,threshold=1e-5)
         h_BL_wth = myTool.find_wth_boundary(w_th)
 
+        # Initialize the plotting tool for the specific case and region
         Plot = VVMPlot_BL(path(case),case_name=case_name[i],region_name=region)
+
+        # Plot the BL height with multiple criteria (lines) and shaded w'theta' data
         Plot.BL_height(
             data_lines=[h_BL_th_plus05, h_BL_dthdz, h_BL_TKE, h_BL_Enstrophy, h_BL_wth[0], h_BL_wth[1], h_BL_wth[2]],
             data_shading=w_th.T,
