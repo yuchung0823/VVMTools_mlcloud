84b57ad4ab0a84bb4593c7a9e3aeb2b37ac5e07d
yuchung0823 <yuzhongzhng@gmail.com>
2024-10-29 20:24:21 +0800

    Updating the VVManalyze function for boundary detection using new plottools

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-from VVManalyze import VVMTools_BL
-from plottools import VVMPlot_BL
 import numpy as np
+from VVManalyze import VVMTools_BL
+from plottools import dataPlotters
+import matplotlib.pyplot as plt
+
+# prepare expname and data coordinate
+nx = 128; x = np.arange(nx)*0.2
+ny = 128; y = np.arange(ny)*0.2
+nz = 50;  z = np.arange(nz)*0.04
+nt = 721; t = np.arange(nt)*np.timedelta64(2,'m')+np.datetime64('2024-01-01 05:00:00')
+time = np.arange(nt)
 
 # Define the path to the simulation data for different cases
 path = lambda case:'/home/chung0823/cloud2024/VVM_Data/%s/'%(case)
@@ -9,32 +17,6 @@ path = lambda case:'/home/chung0823/cloud2024/VVM_Data/%s/'%(case)
 case_list = ['pbl_hetero_dthdz_8','pbl_hetero_dthdz_11','OceanGrass_S1_new','OceanGrass_S1_landmid_new','OceanGrass_S2_new_new','OceanGrass_S2_landmid_newnew']
 case_names = ['S1_Coastal','S2_Coastal','S1_Ocean','S1_Grass','S2_Ocean','S2_Grass']
 
-# Time range for the analysis (721 time steps)
-time = np.arange(721)
-
-############################ Hovmoller diagram ############################
-
-# Configuration for domain range, used to subset the data for analysis
-func_config={"domain_range":(0,1,None,None,None,None)}
-
-# Loop through the first 6 cases to generate Hovmöller diagrams
-for i in range(6):
-    case = case_list[i]
-    case_name = case_names[i]
-
-    # Initialize the analysis and plotting tools
-    myTool = VVMTools_BL(path(case))
-    Plot = VVMPlot_BL(path(case),case_name=case_name,region_name='y-axis avg')
-   
-    # Retrieve NO and NO2 data and calculate their sum (NOx)
-    NO = myTool.get_var_parallel('NO',time_steps=time,domain_range=func_config['domain_range'], compute_mean=True, axis=(0),cores=20)
-    NO2 = myTool.get_var_parallel('NO2',time_steps=time,domain_range=func_config['domain_range'], compute_mean=True, axis=(0),cores=20)
-    NOx = NO + NO2
-   
-    # Plot Hovmöller diagram of NOx data
-    Plot.hovmoller(data=NOx)
-
-############################ BL height ############################
 # Domain configurations for different regions (whole domain, ocean, grass)
 func_configs=[{"domain_range":(None,None,None,None,None,None)}, # Full domain
               {"domain_range":(None,None,None,None,0,64)},      # Ocean region
@@ -49,24 +31,43 @@ for i in range(2):
 
     # Loop through each region (domain, ocean, grass)
     for region, func_config in zip(region_lists, func_configs):
+       
+        expname = case_name[i]
         
-        # Compute TKE (Turbulent Kinetic Energy), Enstrophy, and vertical heat flux (w'theta')
+        # Compute theta, TKE (Turbulent Kinetic Energy), Enstrophy, and vertical heat flux (w'theta')
+        th = myTool.get_var_parallel('th',time_steps=time,domain_range=func_config["domain_range"],compute_mean=True,axis=(1,2),cores=20)
         TKE = myTool.func_time_parallel(func=myTool.calc_TKE,time_steps=time,func_config=func_config,cores=20)
         Enstrophy = myTool.func_time_parallel(func=myTool.calc_Enstrophy,time_steps=time,func_config=func_config,cores=20)
         w_th = myTool.func_time_parallel(func=myTool.calc_w_th,time_steps=time,func_config=func_config,cores=20)
 
         # Calculate boundary layer heights based on different criteria
-        h_BL_th_plus05 = myTool.h_BL_th_plus05(time_steps=time,func_config=func_config)
-        h_BL_dthdz = myTool.h_BL_dthdz(time_steps=time,func_config=func_config)
-        h_BL_TKE = myTool.find_BL_boundary(TKE,threshold=0.08)
-        h_BL_Enstrophy = myTool.find_BL_boundary(Enstrophy,threshold=1e-5)
-        h_BL_wth = myTool.find_wth_boundary(w_th)
+        h_BL_th_plus05 = myTool.find_BL_boundary(th,howToSearch='th_plus05K')
+        h_BL_dthdz = myTool.find_BL_boundary(th,howToSearch='dthdz')
+        h_BL_TKE = myTool.find_BL_boundary(TKE,howToSearch='threshold',threshold=0.08)
+        h_BL_Enstrophy = myTool.find_BL_boundary(Enstrophy,howToSearch='threshold',threshold=1e-5)
 
-        # Initialize the plotting tool for the specific case and region
-        Plot = VVMPlot_BL(path(case),case_name=case_name[i],region_name=region)
+        # create dataPlotter class
+        figpath           = './fig/'
+        data_domain       = {'x':x, 'y':y, 'z':z, 't':t}
+        data_domain_units = {'x':'km', 'y':'km', 'z':'km', 't':'LocalTime'}
+        dplot = dataPlotters(expname, figpath, data_domain, data_domain_units)
 
-        # Plot the BL height with multiple criteria (lines) and shaded w'theta' data
-        Plot.BL_height(
-            data_lines=[h_BL_th_plus05, h_BL_dthdz, h_BL_TKE, h_BL_Enstrophy, h_BL_wth[0], h_BL_wth[1], h_BL_wth[2]],
-            data_shading=w_th.T,
-            )
+        # draw z-t diagram
+        # input data dimension is (nz, nt)
+        # [output] figure, axis, colorbar axis
+        w_th = np.hstack((np.full((nt,1),np.nan), w_th))
+        fig, ax, cax = dplot.draw_zt(data = w_th.T, \
+                                    levels = np.arange(-0.04,0.041,0.005), \
+                                    extend = 'both', \
+                                    pblh_dicts={r'$\theta$ + 0.5 K': h_BL_th_plus05,\
+                                                r'max d$\theta$/dz': h_BL_dthdz,\
+                                                'TKE': h_BL_TKE,\
+                                                'Enstrophy': h_BL_Enstrophy ,\
+                                                },\
+                                    title_left  = r'Vertical $\theta$ transport', \
+                                    title_right = region, \
+                                    figname     = 'BL_height_'+expname+'_'+region,\
+                            )
+
+        plt.show()
+
